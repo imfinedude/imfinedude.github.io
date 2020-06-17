7fad1b2d554f3fc9e5dfd3c16a19a08336c7647c
Muhammad Q Shahzad <mqasims@live.com>
2020-06-17 00:24:01 -0400

    Updates

---
+++ precache-manifest.7fd2042c62e8d353db54302b149e63c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91dbfcd0553d96f7b5f06dc044afbd0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c306b3c9317e35c5798e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c46ddad39999ac3704e",
+    "url": "/static/js/2.7273ba72.chunk.js"
+  },
+  {
+    "revision": "c306b3c9317e35c5798e",
+    "url": "/static/js/main.3bb1aa2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
